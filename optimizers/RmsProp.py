1f89d06b1d29a974a62ddec90d3436b32d2c44fd
Shilu <89338633+shilu10@users.noreply.github.com>
2022-03-13 17:11:22 +0530

    final commit

---
+++ optimizers/RmsProp.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def rmsprop(X, y) :
+    rows, columns = X.shape
+    learning_rate = 1
+    weights = np.zeros(columns)
+    bias = 0
+    epochs = 1000
+    cost = [ ]
+    itera = [ ]
+    prev_update_weight = [0]
+    prev_update_bias = 0
+    c = 0.09
+    gamma = 0.80
+    
+    for _ in range(epochs) :
+        y_p = np.dot(X ,weights) + bias
+        cost1 = np.mean(np.square(y - y_p))
+        
+        dw = -2/rows * (np.dot(X.T, (y - y_p)))
+        db = -2/rows *  sum((y- y_p))
+
+        update_w = (prev_update_weight * gamma) + (1 - gamma) * (dw ** 2) 
+        update_b = (prev_update_bias * gamma ) + (1- gamma ) * (db ** 2)
+
+        prev_update_weights = update_w
+        prev_update_bias = update_b
+        
+        weights = weights - (learning_rate / np.sqrt(c + update_w) ) * dw
+        bias = bias - (learning_rate / np.sqrt(c + update_b) )  * db
+        
+        if _ % 10 == 0 :            
+            cost.append(cost1)
+            itera.append(_)
+    return weights, bias, itera, cost
+
+
+def plotting_rmsprop(X, y) :
+    weight, bias, itera, cost = momentum_based_gradient_descent(X, y)
+    print(f"Weights : {weight}")
+    print(f"bias : {bias}")
+    plt.plot(itera, cost)
